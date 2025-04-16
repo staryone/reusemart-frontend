300c9d4c9ac498c38912c23110211f4e87bed8e6
frendyUAJY <220711608@students.uajy.ac.id>
2025-04-16 22:04:29 +0700

    update main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { Poppins } from 'next/font/google';
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// Configure the font
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'], // add the weights you need
+  variable: '--font-poppins', // optional for using CSS variable
 });
 
 export const metadata: Metadata = {
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { FaCartShopping } from "react-icons/fa6";
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <nav className="bg-white border-b border-gray-200">
         <div className="max-w-screen-xl mx-auto flex items-center justify-between p-4">
           {/* Logo */}
@@ -130,6 +130,35 @@ export default function Home() {
           </div>
         )}
       </nav>
+      <div className="w-screen h-auto">
+        <img src="banner.png" alt="image" className="w-screen" />
+      </div>
+      <div className="flex flex-col items-start justify-items-center px-18 py-18">
+        <h2 className="text-4xl mb-4 mt-10">Barang Terbaru</h2>
+        <div className="flex gap-3">
+          <div className="w-full max-w-3xs bg-white border border-gray-200 rounded-lg shadow-sm">
+            <a href="#">
+              <img
+                className="p-8 rounded-t-lg"
+                src="product.png"
+                alt="product image"
+              />
+            </a>
+            <div className="px-5 pb-5">
+              <a href="#">
+                <h5 className="text-lg tracking-tight text-gray-900">
+                  Apple Watch Series 7 GPS, Aluminium Case
+                </h5>
+              </a>
+              <div className="flex items-center justify-between mt-2">
+                <span className="text-xl font-bold text-gray-900">
+                  Rp2.500.000
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
